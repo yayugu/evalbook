1724dd434d229f5671fb7ac7589813f964d623d6
yayugu <yayugu@gmail.com>
2011-04-21 09:48:14 +0900

    move code which use platex, dvipdfmx, ... from web/main.rb to typesetting/typesetting.rb

---
+++ typesetting/typesetting.rb
@@ -0,0 +1,32 @@
+require 'xml.rb'
+require 'ybook.rb'
+
+def do_command(dirname, command)
+  result = nil
+  Dir.chdir(dirname) do
+    result = system(command)
+    unless result
+      puts "Error: command failed (#{command})"
+      raise Exception
+    end
+  end
+  puts command.to_s
+  Dir.chdir($pwd)
+  return result
+end
+
+def typeset(t, text)
+  parser = Nokogiri::HTML::SAX::Parser.new(HTMLDoc.new(t)).parse(text)
+  filename = "ag-" + sprintf("%05d", rand(100000))
+  template = open($dir_typesetting + '/layout.erb', 'r').read
+  open("#{$dir_tmp}/#{filename}.tex", 'w'){|fp| fp.write(ERB.new(template).result(binding))}
+  open("#{$dir_tmp}/#{filename}.sh", 'w'){|fp| fp.write(<<-"EOF")}
+      platex -interaction=nonstopmode #{$dir_tmp}/#{filename}.tex
+      dvipdfmx -p #{t.width}pt,#{t.height}pt #{$dir_tmp}/#{filename}.dvi
+      mv #{filename}.pdf #{$dir_public_tmp}
+      rm #{filename}.tex #{filename}.dvi
+  EOF
+  do_command($dir_tmp, "sh #{filename}.sh")
+  filename
+end
+
--- web/main.rb
+++ web/main.rb
@@ -5,9 +5,7 @@ require 'haml'
 require 'sinatra'
 
 $LOAD_PATH.push "../typesetting"
-require 'xml.rb'
-require 'ybook.rb'
-
+require 'typesetting.rb'
 
 helpers do
   include Rack::Utils
@@ -19,38 +17,10 @@ helpers do
     "#{request.scheme}://#{request.host}#{port}"
   end
 
-  def do_command(dirname, command)
-    result = nil
-    Dir.chdir(dirname) do
-      result = system(command)
-      unless result
-        puts "Error: command failed (#{command})"
-        raise Exception
-      end
-    end
-    puts command.to_s
-    Dir.chdir($pwd)
-    return result
-  end
-
-  def typeset(t, text)
-    parser = Nokogiri::HTML::SAX::Parser.new(HTMLDoc.new(t)).parse(text)
-    filename = "ag-" + sprintf("%05d", rand(100000))
-    template = open($dir_typesetting + '/layout.erb', 'r').read
-    open("#{$dir_tmp}/#{filename}.tex", 'w'){|fp| fp.write(ERB.new(template).result(binding))}
-    open("#{$dir_tmp}/#{filename}.sh", 'w'){|fp| fp.write(<<-"EOF")}
-      platex -interaction=nonstopmode #{$dir_tmp}/#{filename}.tex
-      dvipdfmx -p #{t.width}pt,#{t.height}pt #{$dir_tmp}/#{filename}.dvi
-      mv #{filename}.pdf ../public/tmp
-      rm #{filename}.tex #{filename}.dvi
-      EOF
-    do_command($dir_tmp, "sh #{filename}.sh")
-    filename
-  end
-
   $pwd = Dir.pwd
   $dir_typesetting =  File.expand_path "../typesetting"
-  $dir_tmp = File.expand_path "./tmp"
+  $dir_tmp = File.expand_path "../tmp"
+  $dir_public_tmp = File.expand_path "./public/tmp"
 end
 
 configure do
